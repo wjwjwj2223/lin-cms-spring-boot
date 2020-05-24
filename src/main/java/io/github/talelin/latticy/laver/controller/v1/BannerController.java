e866fbd881438dffad40bd45291aee76b819e8df
wangjie <17054214@qq.com>
2020-05-24 16:33:43 +0800

    升级后修改配置

--- src/main/java/io/github/talelin/latticy/laver/controller/v1/BannerController.java
+++ src/main/java/io/github/talelin/latticy/laver/controller/v1/BannerController.java
@@ -2,6 +2,7 @@ package io.github.talelin.latticy.laver.controller.v1;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.github.talelin.latticy.common.mybatis.Page;
+import io.github.talelin.latticy.laver.bo.BannerWithItemsBO;
 import io.github.talelin.latticy.laver.dto.BannerDTO;
 import io.github.talelin.latticy.laver.model.BannerDO;
 import io.github.talelin.latticy.laver.service.BannerService;
