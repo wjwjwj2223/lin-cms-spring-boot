1f2dd8f49df20a595f33552bf75d2b4e63232db0
wangjie <17054214@qq.com>
2020-05-24 16:51:32 +0800

    自动生成activity  相关代码

---
+++ src/main/java/io/github/talelin/latticy/laver/mapper/ActivityMapper.java
@@ -0,0 +1,16 @@
+package io.github.talelin.latticy.laver.mapper;
+
+import io.github.talelin.latticy.laver.model.ActivityDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author generator@Wangjie
+ * @since 2020-05-24
+ */
+public interface ActivityMapper extends BaseMapper<ActivityDO> {
+
+}
---
+++ src/main/java/io/github/talelin/latticy/laver/service/ActivityService.java
@@ -0,0 +1,16 @@
+package io.github.talelin.latticy.laver.service;
+
+import io.github.talelin.latticy.laver.model.ActivityDO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author generator@Wangjie
+ * @since 2020-05-24
+ */
+public interface ActivityService extends IService<ActivityDO> {
+
+}
